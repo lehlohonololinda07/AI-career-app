fc2849f67fd8b5086383e51983d1732a3f37b529
lehlohonololinda07 <lehlohonololinda07@gmail.com>
2026-09-24 00:34:35 +0200

    Create app_strings.dart

---
+++ lib/core/constants/app_strings.dart
@@ -0,0 +1,6 @@
+class AppStrings {
+  static const appName = 'AI Career';
+  static const tagline =
+      'Understand where you are. Discover where you could go. '
+      'Build the skills to get there.';
+}
